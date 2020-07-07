7bed68950dc9464e0ca93a2ae495938fbf4793b6
george cox <georgecox67@hotmail.co.uk>
2020-07-07 18:58:51 +0100

    basic user interaction created

--- main.py
+++ main.py
@@ -8,17 +8,27 @@ pygame.display.set_caption("Chess Game")
 
 x = 50
 y = 50
-width = 20
-height = 20
+w = 20
+h = 20
 vel = 5
 
 run = True
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(10)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        x -= vel
+    if keys[pygame.K_RIGHT]:
+        x += vel
+
+    win.fill((0, 0, 0))
+    pygame.draw.rect(win, (255, 0, 0), (x, y, w, h))
+    pygame.display.update()
 
 
 pygame.quit()
