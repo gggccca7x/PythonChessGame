029ffebaf0ca0b04d475e34884d00a61e00d0322
george cox <georgecox67@hotmail.co.uk>
2020-07-07 18:53:52 +0100

    basic pygame window created

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-# Please note: I am using a conda enrivonement...
-
 import pygame
+
+# Please note: I am using a python interpreter with a conda enrivonement...
+
+pygame.init()
+win = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("Chess Game")
+
+x = 50
+y = 50
+width = 20
+height = 20
+vel = 5
+
+run = True
+while run:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+
+pygame.quit()
