9e5a26eb985a96b6f54b8456a935aeab4adf1913
george cox <georgecox67@hotmail.co.uk>
2020-07-08 11:49:22 +0100

    dragging 3 different rectangles

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Chess Game")
 
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
-x = 50
-y = 50
-w = 20
-h = 20
-vel = 5
+x, y, w, h = 50, 50, 20, 20
 
 run = True
 rectangle_draging = False
+
 rectangle = pygame.rect.Rect(x, y, w, h)
+rectangle2 = pygame.rect.Rect(x, y, w, h)
+rectangle3 = pygame.rect.Rect(x, y, w, h)
+
+dragged_rect = rectangle
 
 while run:
     pygame.time.delay(10)
@@ -28,22 +31,32 @@ while run:
             if event.button == 1:            
                 if rectangle.collidepoint(event.pos):
                     rectangle_draging = True
+                    dragged_rect = rectangle
+                elif rectangle2.collidepoint(event.pos):
+                    rectangle_draging = True
+                    dragged_rect = rectangle2
+                elif rectangle3.collidepoint(event.pos):
+                    rectangle_draging = True
+                    dragged_rect = rectangle3
+                if rectangle_draging:
                     mouse_x, mouse_y = event.pos
-                    rectangle.x = mouse_x - rectangle.w/2
-                    rectangle.y = mouse_y - rectangle.h/2
-                    offset_x = rectangle.x - mouse_x
-                    offset_y = rectangle.y - mouse_y
+                    dragged_rect.x = mouse_x - rectangle.w/2
+                    dragged_rect.y = mouse_y - rectangle.h/2
+                    offset_x = dragged_rect.x - mouse_x
+                    offset_y = dragged_rect.y - mouse_y
         elif event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:            
                 rectangle_draging = False
         elif event.type == pygame.MOUSEMOTION:
             if rectangle_draging:
                 mouse_x, mouse_y = event.pos
-                rectangle.x = mouse_x + offset_x
-                rectangle.y = mouse_y + offset_y
+                dragged_rect.x = mouse_x + offset_x
+                dragged_rect.y = mouse_y + offset_y
 
     win.fill((0, 0, 0))
     pygame.draw.rect(win, RED, rectangle)
+    pygame.draw.rect(win, GREEN, rectangle2)
+    pygame.draw.rect(win, BLUE, rectangle3)
     pygame.display.update()
 
 pygame.quit()
