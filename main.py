e6d7f421a23018425a62153a0c5c5fee0afc275d
george cox <georgecox67@hotmail.co.uk>
2020-07-08 11:31:08 +0100

    found code which allows click and drag

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ pygame.init()
 win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Chess Game")
 
+RED   = (255,   0,   0)
+
 x = 50
 y = 50
 w = 20
@@ -13,22 +15,33 @@ h = 20
 vel = 5
 
 run = True
+rectangle_draging = False
+rectangle = pygame.rect.Rect(x, y, w, h)
+
 while run:
     pygame.time.delay(10)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        x -= vel
-    if keys[pygame.K_RIGHT]:
-        x += vel
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:            
+                if rectangle.collidepoint(event.pos):
+                    rectangle_draging = True
+                    mouse_x, mouse_y = event.pos
+                    offset_x = rectangle.x - mouse_x
+                    offset_y = rectangle.y - mouse_y
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:            
+                rectangle_draging = False
+        elif event.type == pygame.MOUSEMOTION:
+            if rectangle_draging:
+                mouse_x, mouse_y = event.pos
+                rectangle.x = mouse_x + offset_x
+                rectangle.y = mouse_y + offset_y
 
     win.fill((0, 0, 0))
-    pygame.draw.rect(win, (255, 0, 0), (x, y, w, h))
+    pygame.draw.rect(win, RED, rectangle)
     pygame.display.update()
-
 
 pygame.quit()
